54f2260f059416517fbd70a16f775aff1de2c073
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/UnboundCheckColumn/Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace UnboundCheckColumn
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            grid.ItemsSource = TestDataList.Create();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            view.AllowSelectionSynchronize = !view.AllowSelectionSynchronize;
+        }
+    }
+
+    public class TestDataList : List<TestDataItem> {
+        public static TestDataList Create() {
+            TestDataList res = new TestDataList();
+            for(int i = 0; i < 1000; i++) {
+                TestDataItem item = new TestDataItem();
+                item.ID = i;
+                item.Value = "A";
+                res.Add(item);
+            }
+            for(int i = 0; i < 1000; i++) {
+                TestDataItem item = new TestDataItem();
+                item.ID = i;
+                item.Value = "B";
+                res.Add(item);
+            }
+            return res;
+        }
+    }
+    public class TestDataItem {
+        public int ID { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to select/unselect all check boxes using the check box in the GridColumn header
+
+
+<p>Starting with version <strong>14.2</strong>, GridControl provides <a href="https://documentation.devexpress.com/WPF/CustomDocument17808.aspx">Selector Column</a> out of the box. To show this column, set the <a href="https://documentation.devexpress.com/WPF/DevExpressXpfGridTableView_ShowCheckBoxSelectorColumntopic.aspx">TableView.ShowCheckBoxSelectorColumn</a> property to true. <br><br>For version older than <strong>14.2</strong>, to achieve this functionality, it is necessary to create a check edit unbound column, place a check edit in the column header, and select/unselect all check boxes using one check box in the header. You can get more information about this approach in the following KB article: <a href="https://www.devexpress.com/Support/Center/p/KA18610">How to implement multiple row selection behavior via a checked column</a>.</p>
+
+<br/>
+
+
