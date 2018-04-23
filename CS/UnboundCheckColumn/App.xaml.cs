036ee02734e8a6a56b02b60518a185347a634d59
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/UnboundCheckColumn/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿// Developer Express Code Central Example:
+// How to implement an unbound editable check column
+// 
+// This example demonstrates how to emulate row selection via an unbound column
+// with a checkbox. The grid's built-in editing is used. It's enabled when the
+// view's NavigationStyle property is set to CellNavigation. Note: If you want this
+// feature to be natively supported by the DXGrid control, please track the
+// corresponding suggestion: .
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E1263
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace UnboundCheckColumn {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ VB/UnboundCheckColumn/Window1.xaml.vb
@@ -0,0 +1,62 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+
+Namespace UnboundCheckColumn
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = TestDataList.Create()
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			view.AllowSelectionSynchronize = Not view.AllowSelectionSynchronize
+		End Sub
+	End Class
+
+	Public Class TestDataList
+		Inherits List(Of TestDataItem)
+		Public Shared Function Create() As TestDataList
+			Dim res As New TestDataList()
+			For i As Integer = 0 To 999
+				Dim item As New TestDataItem()
+				item.ID = i
+				item.Value = "A"
+				res.Add(item)
+			Next i
+			For i As Integer = 0 To 999
+				Dim item As New TestDataItem()
+				item.ID = i
+				item.Value = "B"
+				res.Add(item)
+			Next i
+			Return res
+		End Function
+	End Class
+	Public Class TestDataItem
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+		Private privateValue As String
+		Public Property Value() As String
+			Get
+				Return privateValue
+			End Get
+			Set(ByVal value As String)
+				privateValue = value
+			End Set
+		End Property
+	End Class
+End Namespace
